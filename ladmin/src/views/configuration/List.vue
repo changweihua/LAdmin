a195c4c7d260078e36797445dca044616fdf9ead
changweihua <changweihua@outlook.com>
2019-05-09 09:13:07 +0800

    加入搜索控件

--- ladmin/src/views/configuration/List.vue
+++ ladmin/src/views/configuration/List.vue
@@ -4,6 +4,42 @@
     <el-row style="margin-top:30px;">
       <el-col :span="24">
         <el-card class="box-card">
+          <el-form :model="form" label-width="100px" class="normal-form">
+            <el-row :gutter="20">
+              <el-col :xs="24" :sm="24" :md="12" :lg="12" :xl="12">
+                <el-form-item label="L('启动日期')">
+                  <el-input size="medium" />
+                </el-form-item>
+              </el-col>
+              <el-col :xs="24" :sm="24" :md="12" :lg="12" :xl="12">
+                <el-form-item label="L('启动日期')">
+                  <el-input size="medium" />
+                </el-form-item>
+              </el-col>
+              <el-col :xs="24" :sm="24" :md="12" :lg="12" :xl="12">
+                <el-form-item label="L('启动日期')">
+                  <el-input size="medium" />
+                </el-form-item>
+              </el-col>
+              <el-col :xs="24" :sm="24" :md="12" :lg="12" :xl="12">
+                <el-form-item label="L('启动日期')">
+                  <el-input size="medium" />
+                </el-form-item>
+              </el-col>
+              <el-col :xs="24" :sm="24" :md="12" :lg="12" :xl="12">
+                <el-form-item label="L('启动日期')">
+                  <el-input size="medium" />
+                </el-form-item>
+              </el-col>
+            </el-row>
+          </el-form>
+          <el-row class="btn-wrap">
+            <el-button type="primary" size="small" icon="el-icon-search" @click="handleSearchClick">Search</el-button>
+          </el-row>
+        </el-card>
+      </el-col>
+      <el-col :span="24">
+        <el-card class="box-card">
           <x-table :table-columns="tableColumns" :table-pager="tablePager" :table-actions="tableActions" :table-data="configurationList" :table-query="tableQuery" @size-change="handleSizeChange" @current-change="handleCurrentChange">
             <el-row slot="tools" class="btn-wrap">
               <el-button type="primary" size="small" icon="el-icon-plus" @click="handleCreateClick">Add</el-button>
@@ -111,6 +147,12 @@ export default {
     },
     handleCurrentChange(val) {
       this.tablePager.page = val
+      this.tablePager.skipCount = (this.tablePager.page - 1) * this.tablePager.maxResultCount
+      var query = Object.assign({}, this.tableQuery, this.tablePager)
+      this.fetchList(query)
+    },
+    handleSearchClick() {
+      this.tablePager.page = 1
       this.tablePager.skipCount = (this.tablePager.page - 1) * this.tablePager.maxResultCount
       var query = Object.assign({}, this.tableQuery, this.tablePager)
       this.fetchList(query)
