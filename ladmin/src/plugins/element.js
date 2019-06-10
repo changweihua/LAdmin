c265f79e2d4304164a6ad8142563201675de236a
changweihua <changweihua@outlook.com>
2019-06-10 15:41:36 +0800

    升级axios、elementui

--- ladmin/src/plugins/element.js
+++ ladmin/src/plugins/element.js
@@ -1,5 +1,10 @@
-import Vue from "vue";
-import Element from "element-ui";
-import "../element-variables.scss";
+import Vue from 'vue'
+import Element from 'element-ui'
+import '../element-variables.scss'
 
-Vue.use(Element);
+Vue.use(Element)
+
+// 在引入 Element 时，可以传入一个全局配置对象。
+// 该对象目前支持 size 与 zIndex 字段。
+// size 用于改变组件的默认尺寸，zIndex 设置弹框的初始 z-index（默认值：2000）。
+// Vue.use(Element, { size: 'small', zIndex: 3000 })
